71d58c10c0073ca92a6063ff0516f8582110057c
Ivan <frctyjd.0111@mail.ru>
2022-10-30 22:48:18 +1000

    В выводе 2 книги исправил автора

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Author authorOne = new Author("Александр", " Пушкин");
         Book bookOne = new Book(authorOne, "Капитанская дочка: ", 1836);
-        Author authorTwo = new Author("Лев", "Толстой");
+        Author authorTwo = new Author("Лев", " Толстой");
         Book bookTwo = new Book(authorTwo, "Детсво: ", 1852);
         bookTwo.setYear(1900);// Новая дата второй книги
         System.out.println("Книга 1 - " + bookOne.getTitle() + authorOne.getName() + authorOne.getLastName() +
                 " " + bookOne.getYear());
-        System.out.println("Книга 2 - " + bookTwo.getTitle() + authorOne.getName() + authorOne.getLastName() +
+        System.out.println("Книга 2 - " + bookTwo.getTitle() + authorTwo.getName() + authorTwo.getLastName() +
                 " " + bookTwo.getYear());
         System.out.println("Новая дата книги - " + bookTwo.getYear());
 
